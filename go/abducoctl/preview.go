6145a1529074c90df7a15f34881bc338e3a9a756
root <root@f36.vpnservice.company>
2022-02-04 23:48:56 +0000

    m

--- go/abducoctl/preview.go
+++ go/abducoctl/preview.go
@@ -45,13 +45,11 @@ func DoPreview() {
 				strings.Join(p[i].Executables[1:len(p[i].Executables)], ` `),
 			)
 			return s
-			//len(p[i].Executables)-1]
 		},
 		fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
 			if i == -1 {
 				return ""
 			}
-			//AbducoSession,PPID":1,"PID":213409,"PIDs":[213409,213410],"Threads":2,"Session":"41d42602","Executable":"abduco-sb","Executables":["abduco-sb","bash"],"Started":"2022-02-04 23:26:05","Username":"root","Cmdline":"/usr/local/bin/abduco-sb -L 50 -A 41d42602","Cwd":"/","Status":["sleep"],"ConnectionsQty":1,"OpenFilesQty":5,"Terminal":"","CreateTime":1644017164000,"CPUPercent":0,"MemoryPercent":0.0006608701},{"P
 			return fmt.Sprintf("Session: %s (%s) | PID %d | User %s",
 				p[i].Session,
 				p[i].PID,
