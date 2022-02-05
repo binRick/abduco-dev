82116a18d782655228c0032f0f5dcb661e037515
rick <rick@ricks-MacBook-Air.local>
2022-02-04 22:57:57 -0500

    m

---
+++ go/abducoctl/sb.go
@@ -0,0 +1,41 @@
+package abducoctl
+
+import (
+	"bufio"
+	"fmt"
+)
+
+func Buffer() []byte {
+	cmd := SbList(`a2`)
+	r, _ := cmd.StdoutPipe()
+	//pp.Println(cmd)
+	cmd.Stderr = cmd.Stdout
+	done := make(chan struct{})
+	scanner := bufio.NewScanner(r)
+	lines := []string{}
+	go func() {
+		for scanner.Scan() {
+			line := scanner.Text()
+			lines = append(lines, line)
+		}
+		done <- struct{}{}
+	}()
+	_ = cmd.Start()
+	<-done
+	cmd.Wait()
+	fmt.Println(lines)
+	/*
+		select {
+		case <-time.After(100 * time.Millisecond):
+			if err := cmd.Process.Kill(); err != nil {
+				log.Fatal("failed to kill process: ", err)
+			}
+			log.Println("process killed as timeout reached")
+		case err := <-done:
+			if err != nil {
+				log.Fatalf("process finished with error = %v", err)
+			}
+			log.Print("process finished successfully")
+		}*/
+	return []byte(`xx`)
+}
